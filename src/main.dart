a25fd252e6ae59848f52888829b82ec804bf0e18
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-02 19:48:46 -0400

    Added concept of `currentScreen` to main function

--- src/main.dart
+++ src/main.dart
@@ -6,8 +6,10 @@
 
 import 'score.dart';
 import 'settings.dart';
+import 'screen.dart';
 
 void main() {
+  Screen currentScreen;
   // TODO: Define `IntroScreen`.
   IntroScreen introScreen = new IntroScreen();
   // TODO: Define `SettingsScreen`.
@@ -17,22 +19,30 @@ void main() {
   // TODO: Define `EndScreen`.
   EndScreen endScreen = new EndScreen();
 
+  CanvasElement canvas = querySelector("#mycanvas");
+
   Score latestScore = new Score(0, 0);
   Settings currentSettings = new Settings();
   bool changeSettings = true;
 
+  currentScreen = introScreen;
+  canvas.onClick.listen((e) => currentScreen.handleClick(e));
   introScreen.execute();
+
   while (true) {
     if (changeSettings) {
+      currentScreen = settingsScreen;
       settingsScreen.execute(currentSettings);
       currentSettings = settingsScreen.getSettings();
       changeSettings = false;
     }
 
+    currentScreen = gameScreen;
     gameScreen.execute(currentSettings);
     latestScore = gameScreen.getScore();
     gameScreen.reset();
 
+    currentScreen = endScreen;
     endScreen.setScore(latestScore);
     endScreen.setIsTwoPlayer(currentSettings.isTwoPlayer);
     endScreen.execute();
