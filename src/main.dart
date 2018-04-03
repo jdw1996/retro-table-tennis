9e8474bc50b770278322df59f4246506d66d16bc
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-02 20:51:22 -0400

    Refactored main functionality into a class

--- src/main.dart
+++ src/main.dart
@@ -8,44 +8,75 @@ import 'score.dart';
 import 'settings.dart';
 import 'screen.dart';
 
-void main() {
-  Screen currentScreen;
-  // TODO: Define `IntroScreen`.
-  IntroScreen introScreen = new IntroScreen();
-  // TODO: Define `SettingsScreen`.
-  SettingsScreen settingsScreen = new SettingsScreen();
-  // TODO: Define `GameScreen`.
-  GameScreen gameScreen = new GameScreen();
-  // TODO: Define `EndScreen`.
-  EndScreen endScreen = new EndScreen();
+class Main {
+  // Minimum time in milliseconds between game and screen updates.
+  static const num _GAME_UPDATE_INTERVAL = 50;
 
-  CanvasElement canvas = querySelector("#mycanvas");
+  // Canvas to draw the game on.
+  CanvasElement _canvas;
+  CanvasRenderingContext2D _canvasContext;
 
-  Score latestScore = new Score(0, 0);
-  Settings currentSettings = new Settings();
-  bool changeSettings = true;
+  // Objects containing logic for different display modes.
+  IntroScreen _introScreen;
+  SettingsScreen _settingsScreen;
+  GameScreen _gameScreen;
+  EndScreen _endScreen;
 
-  currentScreen = introScreen;
-  canvas.onClick.listen((e) => currentScreen.handleClick(e));
-  introScreen.execute();
+  // Reference to whichever `Screen` is currently being displayed.
+  Screen _currentScreen;
 
-  while (true) {
-    if (changeSettings) {
-      currentScreen = settingsScreen;
-      settingsScreen.execute(currentSettings);
-      currentSettings = settingsScreen.getSettings();
-      changeSettings = false;
+  // Score of the most recently finished game.
+  Score _latestScore;
+
+  // Current game settings.
+  Settings _currentSettings;
+
+  // Last time the screen and game state were updated.
+  num _lastUpdateTime = 0;
+
+  // Constructor.
+  Main() {
+    canvas = querySelector("#mycanvas")..focus();
+
+    // TODO: Define `IntroScreen`.
+    _introScreen = new IntroScreen(_canvas);
+    // TODO: Define `SettingsScreen`.
+    _settingsScreen = new SettingsScreen(_canvas);
+    // TODO: Define `GameScreen`.
+    _gameScreen = new GameScreen(_canvas);
+    // TODO: Define `EndScreen`.
+    _endScreen = new EndScreen(_canvas);
+
+    _currentScreen = _introScreen;
+  }
+
+  // Clear the canvas.
+  void clear() {
+    _canvasContext
+      ..fillStyle = "black"
+      ..fillRect(0, 0, _canvas.width, _canvas.height);
+  }
+
+  // Advance all game mechanics and display the game.
+  void update(num currentTime) {
+    final num delta = currentTime - _lastUpdateTime;
+
+    if (delta > _GAME_UPDATE_INTERVAL) {
+      _lastUpdateTime = currentTime;
+      _clear();
+      _currentScreen.updateAndDraw();
     }
 
-    currentScreen = gameScreen;
-    gameScreen.execute(currentSettings);
-    latestScore = gameScreen.getScore();
-    gameScreen.reset();
-
-    currentScreen = endScreen;
-    endScreen.setScore(latestScore);
-    endScreen.setIsTwoPlayer(currentSettings.isTwoPlayer);
-    endScreen.execute();
-    changeSettings = endScreen.mustChangeSettings();
+    // Continue the game loop.
+    run();
   }
+
+  // Begin the game.
+  void run() {
+    window.animationFrame.then(update);
+  }
+}
+
+void main() {
+  new Main()..run();
 }
