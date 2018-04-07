5515d22a1b394fabbb1f6031a6bcbd92f2b05ffc
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-07 15:10:53 -0400

    Improved documentation of `Settings` class

--- src/settings.dart
+++ src/settings.dart
@@ -5,9 +5,12 @@
 //
 
 class Settings {
+  // If `true`, there are two human players in the game; else one, plus an AI.
   bool isTwoPlayer;
+  // Score to play to; first to reach it wins the game.
   int playToScore;
 
+  // Constructor.
   Settings(bool isTwoPlayer, int playToScore)
       : isTwoPlayer = isTwoPlayer,
         playToScore = playToScore;
