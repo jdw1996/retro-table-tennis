da807a36cd607e56a8280dbdef1c29c770876498
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-09 21:57:44 -0400

    Started work on `AIPlayer`

---
+++ src/aiplayer.dart
@@ -0,0 +1,24 @@
+//
+// Joseph Winters
+// AI-controlled paddle
+// April 2018
+//
+
+import 'ball.dart';
+import 'player.dart';
+
+class AIPlayer extends Player {
+  // Constructor.
+  AIPlayer(int canvasHeight, Ball ball) : super(canvasHeight, ball);
+
+  // Return a list containing the x- and y-coordinates of the top-left corner.
+  List<int> getTopLeftCoordinates() {
+    // TODO: Implement.
+  }
+
+  // Determine the best direction to move in and move.
+  @override
+  void move() {
+    // TODO: Implement.
+  }
+}
