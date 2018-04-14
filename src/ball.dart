5e5effe446d116100b433d07868c9bde5d307509
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-14 16:20:00 -0400

    Implemented `move` method for `Ball`

--- src/ball.dart
+++ src/ball.dart
@@ -15,6 +15,8 @@ class Ball {
   // Radius of the ball.
   static const int RADIUS = 50;
 
+  // The maximum possible y-coordinate.
+  final int _canvasHeight;
   // X-coordinate for the centre of the ball.
   int _x = 500;
   // Y-coordinate for the centre of the ball.
@@ -31,7 +33,7 @@ class Ball {
   Random random;
 
   // Constructor.
-  Ball() {
+  Ball(int canvasHeight) : _canvasHeight = canvasHeight {
     random = new Random();
     _currentAngle = Angle.values[random.nextInt(Angle.values.length)];
     if (_currentAngle == Angle.STRAIGHT) {
@@ -39,6 +41,7 @@ class Ball {
     } else {
       _verticalFactor = [-1, 1][random.nextInt(2)];
     }
+    _horizontalFactor = [-1, 1][random.nextInt(2)];
   }
 
   // Return a list containing the x- and y-coordinates of the centre.
@@ -48,6 +51,22 @@ class Ball {
 
   // Move the ball.
   void move() {
-    // TODO: Implement.
+    if (_currentAngle = Angle.STRAIGHT) {
+      _x += _speed * _horizontalFactor;
+      return;
+    }
+
+    num angle = _currentAngle == Angle.STEEP ? PI/4 : PI/8;
+    _x += (cos(angle) * _speed).round();
+    _y += (sin(angle) * _speed).round();
+    if (_y + RADIUS > _canvasHeight) {
+      // Reflect the ball back over the bottom of the canvas.
+      _y = (2 * _canvasHeight) - _y - (2 * RADIUS);
+      _verticalFactor = -1;
+    } else if (_y - RADIUS < 0) {
+      // Reflect the ball back over the top of the canvas.
+      _y = (2 * RADIUS) - _y;
+      _verticalFactor = 1;
+    }
   }
 }
