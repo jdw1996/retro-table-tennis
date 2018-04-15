97e1b5f59adf434b8f81440e6d6ef712ee8f30b9
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-15 14:07:07 -0400

    Implemented `reset` for `Ball`

--- src/ball.dart
+++ src/ball.dart
@@ -12,17 +12,23 @@ class Ball {
   // TODO: Adjust these numbers as necessary.
   // Radius of the ball.
   static const int RADIUS = 50;
+  // Initial x-coordinate for the centre of the ball.
+  static const int _INITIAL_X = 500;
+  // Initial y-coordinate for the centre of the ball.
+  static const int _INITIAL_Y = 250;
+  // Initial number of pixel-widths to move diagonally each iteration.
+  static const int _INITIAL_SPEED = 10;
 
   // The maximum possible y-coordinate.
   final int _canvasHeight;
   // X-coordinate for the centre of the ball.
-  int _x = 500;
+  int _x = _INITIAL_X;
   // Y-coordinate for the centre of the ball.
-  int _y = 250;
+  int _y = _INITIAL_Y;
   // Angle at which the ball is currently travelling.
   Angle _currentAngle;
   // Number of pixel-widths to move diagonally each iteration.
-  int _speed = 10;
+  int _speed = _INITIAL_SPEED;
   // Should be `1` if the ball is moving down; `0` if horizontal; else `-1`.
   int _verticalFactor;
   // Should be `1` if the ball is moving right; else `-1`.
@@ -33,6 +39,11 @@ class Ball {
   // Constructor.
   Ball(int canvasHeight) : _canvasHeight = canvasHeight {
     random = new Random();
+    _randomizeAngle();
+  }
+
+  // Change movement to a random angle.
+  void _randomizeAngle() {
     _currentAngle = Angle.values[random.nextInt(Angle.values.length)];
     if (_currentAngle == Angle.STRAIGHT) {
       _verticalFactor = 0;
@@ -66,5 +77,13 @@ class Ball {
       _y = (2 * RADIUS) - _y;
       _verticalFactor = 1;
     }
+  }
+
+  // Return the ball to the centre of the screen.
+  void reset() {
+    _x = _INITIAL_X;
+    _y = _INITIAL_Y;
+    _speed = _INITIAL_SPEED;
+    _randomizeAngle();
   }
 }
