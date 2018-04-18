6e4f1710e8cc12ae9f53d0c7f285dd65d9243028
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-17 22:39:07 -0400

    Implemented temporary heuristic for `AIPlayer`

--- src/aiplayer.dart
+++ src/aiplayer.dart
@@ -10,10 +10,20 @@ import 'player.dart';
 class AIPlayer extends Player {
   // Constructor.
   AIPlayer(int canvasHeight, Ball ball) : super(canvasHeight, ball);
+  // TODO: Remove this.
+  int _currentDirection = 1;
 
   // Determine the best direction to move in and move.
   @override
   void move() {
-    // TODO: Implement.
+    // TODO: Implement actual functionality.
+    _top += _currentDirection * _PADDLE_SPEED;
+    if (_top < 0) {
+      _top = 0;
+      _currentDirection *= -1;
+    } else if (_top + PADDLE_HEIGHT > _canvasHeight) {
+      _top = _canvasHeight - PADDLE_HEIGHT;
+      _currentDirection *= -1;
+    }
   }
 }
