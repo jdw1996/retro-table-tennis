3c5f37cee39c02b979d625a405fd0030b45106f0
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-18 22:00:42 -0400

    Finished renaming `Screen` class to `Display`

--- src/display.dart
+++ src/display.dart
@@ -4,23 +4,23 @@
 // March 2018
 //
 
-abstract class Screen {
+abstract class Display {
   // Canvas to draw the game on.
   final CanvasElement _canvas;
   // Drawing context for the canvas.
   final CanvasRenderingContext2D _canvasContext;
   // Constructor.
-  Screen(CanvasElement canvas)
+  Display(CanvasElement canvas)
       : _canvas = canvas,
         _canvasContext = canvas.getContext("2d");
   // Execute any necessary game actions and draw the new state on the canvas.
   void updateAndDraw();
-  // Return `true` if it's time to transition to the next screen; else `false`.
+  // Return `true` if it's time to transition to the next display; else `false`.
   bool isDone();
-  // Return the `Screen` to its original state.
+  // Return the `Display` to its original state.
   void reset();
   // Clear the canvas.
-  void _clearScreen() {
+  void _clearDisplay() {
     _canvasContext
       ..fillStyle = "black"
       ..fillRect(0, 0, _canvas.width, _canvas.height);
