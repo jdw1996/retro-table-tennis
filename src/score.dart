59b71ccbe405c530560467659959b05f41670e27
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-04-18 22:21:59 -0400

    Implemented `reset` method for `Score`

--- src/score.dart
+++ src/score.dart
@@ -8,7 +8,14 @@ class Score {
   int score1;
   int score2;
 
+  // Constructor.
   Score(score1, score2)
       : score1 = score1,
         score2 = score2;
+
+  // Reset the score to 0-0.
+  void reset() {
+    score1 = 0;
+    score2 = 0;
+  }
 }
