0f549a5cfad6a5bb52338b184da11c846fa40a06
Joseph Daniel Winters <jdwinters96@gmail.com>
2018-05-26 16:22:33 -0400

    Implemented `EndDisplay` functionality

--- src/enddisplay.dart
+++ src/enddisplay.dart
@@ -11,6 +11,29 @@ import 'display.dart';
 import 'score.dart';
 
 class EndDisplay extends Display {
+  // Messages to display about the winner.
+  static const String PLAYER1_VICTORY_MESSAGE = "Player 1 has won!";
+  static const String PLAYER2_VICTORY_MESSAGE = "Player 2 has won!";
+  static const String HUMAN_VICTORY_MESSAGE = "You won the game!";
+  static const String AI_VICTORY_MESSAGE = "You lost the game…";
+
+  // Text for the button to play again.
+  static const String PLAY_AGAIN_TEXT = "Play Again";
+  // Text for the button to change settings.
+  static const String CHANGE_SETTINGS_TEXT = "Change Settings";
+
+  // Dimensions of buttons.
+  static const int PLAY_AGAIN_BUTTON_WIDTH = 270;
+  static const int PLAY_AGAIN_BUTTON_HEIGHT = 80;
+  static const int CHANGE_SETTINGS_BUTTON_WIDTH = 390;
+  static const int CHANGE_SETTINGS_BUTTON_HEIGHT = 80;
+
+  // Coordinates of buttons.
+  static const int PLAY_AGAIN_BUTTON_X = 50;
+  static const int PLAY_AGAIN_BUTTON_Y = 350;
+  static const int CHANGE_SETTINGS_BUTTON_X = 560;
+  static const int CHANGE_SETTINGS_BUTTON_Y = PLAY_AGAIN_BUTTON_Y;
+
   // If `true`, it's time to transition to the next display.
   bool _isDone = false;
   // For getting clicks.
@@ -27,16 +50,68 @@ class EndDisplay extends Display {
       : super(canvas),
         _mouse = mouse;
 
+  // Generate the message to display about the winner.
+  String _getVictoryMessage() {
+    if (_isTwoPlayer) {
+      return (_latestScore.score1 > _latestScore.score2)
+          ? PLAYER1_VICTORY_MESSAGE
+          : PLAYER2_VICTORY_MESSAGE;
+    } else {
+      return (_latestScore.score1 > _latestScore.score2)
+          ? HUMAN_VICTORY_MESSAGE
+          : AI_VICTORY_MESSAGE;
+    }
+  }
+
   // Perform logical updates.
   void _update() {
-    // TODO: Implement actual functionality.
-    if (_mouse.hasNewClick()) _isDone = true;
+    if (!_mouse.hasNewClick()) return;
+    List<int> mouseCoordinates = _mouse.getClickCoordinates();
+    int mouseX = mouseCoordinates[0];
+    int mouseY = mouseCoordinates[1];
+    if ((mouseX >= PLAY_AGAIN_BUTTON_X) &&
+        (mouseX <= PLAY_AGAIN_BUTTON_X + PLAY_AGAIN_BUTTON_WIDTH) &&
+        (mouseY >= PLAY_AGAIN_BUTTON_Y) &&
+        (mouseY <= PLAY_AGAIN_BUTTON_Y + PLAY_AGAIN_BUTTON_HEIGHT)) {
+      _mustChangeSettings = false;
+      _isDone = true;
+    } else if ((mouseX >= CHANGE_SETTINGS_BUTTON_X) &&
+        (mouseX <= CHANGE_SETTINGS_BUTTON_X + CHANGE_SETTINGS_BUTTON_WIDTH) &&
+        (mouseY >= CHANGE_SETTINGS_BUTTON_Y) &&
+        (mouseY <= CHANGE_SETTINGS_BUTTON_Y + CHANGE_SETTINGS_BUTTON_HEIGHT)) {
+      _mustChangeSettings = true;
+      _isDone = true;
+    }
   }
 
   // Display the updated state on the canvas.
   void _display() {
     clearDisplay();
-    // TODO: Implement actual functionality.
+    // Display victory message.
+    canvasContext
+      ..font = Display.FONT
+      ..fillStyle = (_latestScore.score1 > _latestScore.score2
+          ? Display.PLAYER1_COLOUR
+          : Display.PLAYER2_COLOUR)
+      ..fillText(_getVictoryMessage(), 300, 150);
+    // Display button to play again.
+    canvasContext
+      ..fillStyle = Display.BUTTON_COLOUR
+      ..fillRect(PLAY_AGAIN_BUTTON_X, PLAY_AGAIN_BUTTON_Y,
+          PLAY_AGAIN_BUTTON_WIDTH, PLAY_AGAIN_BUTTON_HEIGHT)
+      ..font = Display.FONT
+      ..fillStyle = Display.BALL_COLOUR
+      ..fillText(PLAY_AGAIN_TEXT, (PLAY_AGAIN_BUTTON_X + 25),
+          (PLAY_AGAIN_BUTTON_Y + PLAY_AGAIN_BUTTON_HEIGHT - 25));
+    // Display button to change settings.
+    canvasContext
+      ..fillStyle = Display.BUTTON_COLOUR
+      ..fillRect(CHANGE_SETTINGS_BUTTON_X, CHANGE_SETTINGS_BUTTON_Y,
+          CHANGE_SETTINGS_BUTTON_WIDTH, CHANGE_SETTINGS_BUTTON_HEIGHT)
+      ..font = Display.FONT
+      ..fillStyle = Display.BALL_COLOUR
+      ..fillText(CHANGE_SETTINGS_TEXT, (CHANGE_SETTINGS_BUTTON_X + 25),
+          (CHANGE_SETTINGS_BUTTON_Y + CHANGE_SETTINGS_BUTTON_HEIGHT - 25));
   }
 
   // Execute any necessary game actions and draw the new state on the canvas.
